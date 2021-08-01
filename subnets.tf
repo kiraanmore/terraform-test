5499380c7a2ae63172a18de31ddebed7421fc421
EC2 Default User <ec2-user@ip-172-31-12-251.us-east-2.compute.internal>
2021-08-01 19:27:09 +0000

    test

--- subnets.tf
+++ subnets.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "subnet1" {
     Name = "Main"
   }
 }
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Subnet2"
+  }
+}
+
